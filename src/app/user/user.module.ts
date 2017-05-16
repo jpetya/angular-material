b7b7c473f87c05c4b072576071666872ee4465a5
Peter Juhasz <pjuhasz01@gmail.com>
2017-05-16 12:52:53 +0200

    set up routers

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user.routing';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    UserRoutingModule
   ],
   declarations: [LoginComponent]
 })
--- src/app/user/user.routing.ts
+++ src/app/user/user.routing.ts
@@ -1,3 +1,16 @@
-/**
- * New typescript file
- */
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './components/login/login.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class UserRoutingModule { }
